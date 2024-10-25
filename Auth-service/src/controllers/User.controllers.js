1dcde4349e372b001687b3b2998562b687966670
manish sharma dev <manishvsharma1@gmail.com>
2024-10-24 14:18:49 -1200

    Authservice - addedd loginUser controllers

--- Auth-service/src/controllers/User.controllers.js
+++ Auth-service/src/controllers/User.controllers.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { Apiresponse } from "../utils/Apiresponse.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const RegisterUser = async (req,res) => {
     // steps to Register User
@@ -68,6 +69,75 @@ const RegisterUser = async (req,res) => {
     }
 }
 
+const LoginUser = async(req,res) => {
+    //steps to login User 
+    // 1 - take the credential from the user
+    // 2 - Chack validation of it 
+    // 3 - Check if User exits in the db or not
+    // 4 - matched the hashed password with the unhashed password 
+    // 5 - check if the details are matched or not - if not return message 
+          // + if yes 
+               // - generate an accesstoken 
+               // - return the acces token in the user session
+
+    const { email, password } = await req.json()
+
+    if(password.length === 0 && email.length === 0){
+        return new ApiError(400,"Invalid fields, - Fields must be present")
+    }
+
+    const existedUser = await User.findOne({
+        email : email,
+        password : password
+    })
+
+    if(!existedUser){
+        return new ApiError("Invalid field User Does not Exists")
+    }
+
+    const isPasswordCorrect  = await bcrypt.compare(password,existedUser?.password)
+
+    if(isPasswordCorrect === true){
+        return new ApiError(404,"Password Doesn't match")
+    }
+
+    const logedinUser = await User.findById(existedUser?._id).select(" -password ")
+
+    const accesstoken = await jwt.sign(
+        {
+            _id : logedinUser?._id
+        },
+        process.env.JWT_SECRET_KEY,
+        {
+             expireIn : process.env.JWT_EXPIRE_KEY
+        }
+    )
+
+
+    if(accesstoken.length === 0){
+        return new ApiError(
+            500,
+            "Failed in - generating accesstoken"
+        )
+    }
+
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+
+    return res
+    .status(200)
+    .cookie("accesstoken",accesstoken,options)
+    .json(
+        200,
+        "User Logged in - Successfully",
+        logedinUser
+    )
+}
+
 export { 
-    RegisterUser
+    RegisterUser,
+    LoginUser
 }
--- Auth-service/src/routes/User.route.js
+++ Auth-service/src/routes/User.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { RegisterUser } from "../controllers/User.controllers.js"
+import { RegisterUser,LoginUser } from "../controllers/User.controllers.js"
 
 
 const router = Router()
 
 
 router.route('/api/registerUser').post(RegisterUser)
+router.route('/api/loginuser').post(LoginUser)
 
 
 export {
